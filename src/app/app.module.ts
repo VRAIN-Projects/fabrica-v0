148569a9c1fead0d8a90bc09d8843cbcc788b388
Sinnan <jose.ferrus.94@gmail.com>
2023-10-10 09:54:19 +0200

    cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { WorkerComponent } from './worker/worker.component';
-import { ItemComponent } from './item/item.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { FactoryComponent } from './factory/factory.component';
-import { TaskComponent } from './task/task.component';
+import { WorkerComponent } from './components/worker/worker.component';
+import { ItemComponent } from './components/item/item.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FactoryComponent } from './components/factory/factory.component';
+import { TaskComponent } from './components/task/task.component';
 import { DragDropModule } from '@angular/cdk/drag-drop';
-import { SingleWorkerPlaceComponent } from './single-worker-place/single-worker-place.component';
-import { MultipleWorkerPlaceComponent } from './multiple-worker-place/multiple-worker-place.component';
+import { SingleWorkerPlaceComponent } from './components/single-worker-place/single-worker-place.component';
+import { MultipleWorkerPlaceComponent } from './components/multiple-worker-place/multiple-worker-place.component';
 
 @NgModule({
   declarations: [
