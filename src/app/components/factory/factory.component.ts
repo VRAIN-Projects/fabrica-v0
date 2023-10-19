369063e1efbbc488c3f7206fd0bda12d0bd3c1cf
Sinnan <jose.ferrus.94@gmail.com>
2023-10-19 09:38:16 +0200

    Inicio de canvas

--- src/app/components/factory/factory.component.ts
+++ src/app/components/factory/factory.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-factory',
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./factory.component.css']
 })
 export class FactoryComponent {
+  @ViewChild('myCanvas')
+  canvas: ElementRef<HTMLCanvasElement> | undefined = undefined;
 
-}
+  context: CanvasRenderingContext2D | undefined = undefined;
+
+  ngAfterViewInit(): void {
+    if(this.canvas != undefined){
+    
+      this.context = this.canvas?.nativeElement.getContext('2d')!;
+
+    } else {
+      console.log("Error: No se pudo obtener el canvas");
+    }
+    //Draw a line
+    
+    if(this.context != undefined) {
+      
+      //Draw an arrow
+      this.context.beginPath();
+      this.context.moveTo(150, 175);
+      this.context.lineTo(100, 100);
+      //stroke width
+      this.context.lineWidth = 10;
+      this.context.stroke();
+      this.context.closePath();
+      
+    } else {
+      console.log("Error: No se pudo obtener el contexto");
+    }
+
+    
+  }
+
+  }
