6ef5b3bcd2a92d46e827e148b957d34526149839
Sinnan <jose.ferrus.94@gmail.com>
2023-10-20 13:13:32 +0200

    Adjusting title pages

--- src/app/components/statistics/statistics.component.ts
+++ src/app/components/statistics/statistics.component.ts
@@ -9,30 +9,30 @@ import * as echarts from 'echarts';
 })
 export class StatisticsComponent {
 
-  option: any;
-  chartDom: any;
-  myChart: any;
-
   constructor() { 
   }
   ngOnInit(): void {
-    this.option = {
-      xAxis: {
-        type: 'category',
-        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-      },
-      yAxis: {
-        type: 'value'
-      },
-      series: [
-        {
-          data: [150, 230, 224, 218, 135, 147, 260],
-          type: 'line'
-        }
-      ]
-    };
 
-    this.chartDom = document.getElementById('main');
-    this.myChart = echarts.init(this.chartDom);
-  }    
+// Create the echarts instance
+var myChart = echarts.init(document.getElementById('chart'));
+
+// Draw the chart
+myChart.setOption({
+  title: {
+    text: 'ECharts Getting Started Example'
+  },
+  tooltip: {},
+  xAxis: {
+    data: ['shirt', 'cardigan', 'chiffon', 'pants', 'heels', 'socks']
+  },
+  yAxis: {},
+  series: [
+    {
+      name: 'sales',
+      type: 'bar',
+      data: [5, 20, 36, 10, 10, 20]
+    }
+  ]
+});
+  }
 }
