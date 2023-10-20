c737306f62cf461d1262ca1cea224df9f915335c
Sinnan <jose.ferrus.94@gmail.com>
2023-10-20 12:47:48 +0200

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { StatisticsComponent } from './components/statistics/statistics.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'statistics', component: StatisticsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
