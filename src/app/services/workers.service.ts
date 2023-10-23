61c547c9e3991dab407dd6f15683f56600515961
Sinnan <jose.ferrus.94@gmail.com>
2023-10-23 12:14:05 +0200

    Connectors svg

--- src/app/services/workers.service.ts
+++ src/app/services/workers.service.ts
@@ -26,13 +26,16 @@ export class WorkersService {
 
   private actions: Action[] = [
     new Action(1, [this.allSkills[0]], 10, 10),
-    new Action(2, [this.allSkills[1], this.allSkills[3]], 10, 300),
+    new Action(2, [this.allSkills[1], this.allSkills[3]], 10, 310),
     new Action(3, [this.allSkills[0], this.allSkills[4]], 10, 610),
-    new Action(4, [this.allSkills[0], this.allSkills[1], this.allSkills[2]], 10, 900),
-    new Action(5, [this.allSkills[1]], 150, 140),
-    new Action(6, [this.allSkills[2]], 150, 610),
-    new Action(7, [this.allSkills[3]], 290, 140),
-    new Action(8, [this.allSkills[4]], 290, 610),
+    new Action(4, [this.allSkills[0], this.allSkills[1], this.allSkills[2]], 10, 910),
+    new Action(5, [this.allSkills[0], this.allSkills[1], this.allSkills[2]], 10, 1210),
+    new Action(6, [this.allSkills[1]], 150, 140),
+    new Action(7, [this.allSkills[2]], 150, 760),
+    new Action(8, [this.allSkills[2]], 150, 1210),
+    new Action(9, [this.allSkills[3]], 290, 10),
+    new Action(10, [this.allSkills[4]], 290, 310),
+    new Action(11, [this.allSkills[4]], 290, 860),
 
   ];
   
